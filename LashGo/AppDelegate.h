c25ad443fe937cfca8babe75dca21dbd72c1cc60
Vitaliy Pykhtin <admin@Mac-Admin.local>
2014-04-19 19:08:18 +0700

    Initial Commit

---
+++ LashGo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LashGo
+//
+//  Created by Vitaliy Pykhtin on 19.04.14.
+//  Copyright (c) 2014 Vitaliy Pykhtin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
